b1c00dbf74f1adb4dba6cae3a5f03c208a0afbd4
AlexMotajp <alexandre.nike10@hotmail.com>
2019-02-25 17:58:17 -0300

    Exercicios e algumas aulas

---
+++ AulasPythonCursoEmVideo/ex001.py
@@ -0,0 +1,11 @@
+# Olá Mundo... Faça um Olá mundo!
+# Lembrando que é mais interessante usar aspas simples, por convenção
+
+print ('Olá mundo');
+
+# uma outra forma de fazer mais custosa...
+
+mensagem = 'Olá, mundo!'
+
+print(mensagem);
+
---
+++ AulasPythonCursoEmVideo/ex002.py
@@ -0,0 +1,9 @@
+# Crie um input e responda pra o usuário algo.
+
+nome = input('Qual teu nome? ');
+print('é um prazer', nome);
+
+# pode ser feito com format também...
+
+nome = input('Qual teu nome? ');
+print('é um prazer, {}.'.format(nome));
