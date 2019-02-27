ff497b8a3fc18ef294449e0715b1c3f72298a356
AlexMotajp <alexandre.nike10@hotmail.com>
2019-02-27 10:33:42 -0300

    aula 06 input, type e is

---
+++ AulasPythonCursoEmVideo/aula06a.py
@@ -0,0 +1,14 @@
+# mexendo com tipos
+# veja que estou convertendo tudo que está em n1 em inteiro
+# caso não seja colocado o conversor int, ele vai concatenar as strings e não somar
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+
+soma = n1 + n2
+print('O soma entre {0} e {1} deu: {2}'.format(n1, n2, soma))
+# perceba como é interessante o format em conjunto com o print
+
+
+
+
---
+++ AulasPythonCursoEmVideo/aula06b.py
@@ -0,0 +1,13 @@
+# Brincando com os tipos
+# String = str
+#testTipo = str(input('Digite um valor: '))
+#testTipo1 = bool(input('Digite um valor: '))
+
+#testTipo2 = input()
+
+#print(type(testTipo))
+
+# testando algumas tipos com is
+
+n = input('digite algo: ')
+print(n.isdigit())
