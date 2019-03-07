23d790b4bf0d65da83b178e21f63f06c457625a4
AlexMotajp <alexandre.nike10@hotmail.com>
2019-03-07 17:41:44 -0300

    Aula 007 (Curso em Vídeo)

---
+++ AulasPythonCursoEmVideo/aula07a.py
@@ -0,0 +1,37 @@
+# Aula 07 Python com Gustavo Guanabara (Curso Em Vídeo)
+# Brincando com os operadores
+
+# Perceba que o python não tem limite pra tamanho
+# na verdade, seu limite é definido pela memória
+
+print(365**522)
+
+# usando uma função para exponencial
+
+print(365**522)
+
+# perceba as concatenações
+
+print('oi' + 'oi')
+
+print('oi'*10)
+# perceba que ele multiplica a quantidade de 'ois'
+
+# mexer
+
+n1 = 10
+n2 = 20
+
+soma = n1 + n2
+divisa = n1 / n2
+divisaModulo = n1 % n2
+multiplica = n1 * n2
+exponencial = n1 ** n2
+
+print('soma é: {}, divisao é {:.2f}, produto é: {}'.format(soma, divisa, multiplica), end='')
+print('divisãoModulo é: {} e exponencial é: {}'.format(divisaModulo, exponencial))
+
+# Esse 'end' no final evita quebrar linha
+# Caso você queira quebrar propositalmente coloque /n
+
+#Faça os exercicios de 005 ao 015
