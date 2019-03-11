d2c27def5c8ca5109ab0c66011f08c20f8f63915
AlexMotajp <alexandre.nike10@hotmail.com>
2019-03-11 18:00:40 -0300

    Alterando ex12 e ex13

--- AulasPythonCursoEmVideo/ex012.py
+++ AulasPythonCursoEmVideo/ex012.py
@@ -1,6 +1,6 @@
 # Faça um algoritmo que leia o preço de um produto, e altere o mesmo colocando 5% de desconto
 
-produtoInput = float(input("Insira o valor do produto: "))
+produtoInput = float(input("Insira o valor do produto: R$"))
 
-print('O valor do produto: {0}'.format(produtoInput * 0.95))
+print('O valor do produto: R${:.2f}'.format(produtoInput * 0.95))
 
--- AulasPythonCursoEmVideo/ex013.py
+++ AulasPythonCursoEmVideo/ex013.py
@@ -1,6 +1,6 @@
 # Um algoritmo que leia o salário de um funcionário e mostre seu novo com 15% de aumento
 
-salárioFunc = float(input('Qual o seu salário: '))
+salárioFunc = float(input('Qual o seu salário: R$'))
 
-print('seu novo salário é {}'.format(salárioFunc * 1.15))
+print('seu novo salário é R${:.2f}'.format(salárioFunc * 1.15))
 
